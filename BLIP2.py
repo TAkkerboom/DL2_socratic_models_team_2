d037ad12def4ee6a903498a753a98a88a37a0746
TAkkerboom <theodoor.akkerboom@wur.nl>
2023-04-24 17:18:21 +0200

    BLIP2 test

--- BLIP2.py
+++ BLIP2.py
@@ -11,7 +11,7 @@ model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b
 # device = "cuda" if torch.cuda.is_available() else "cpu"
 # model.to(device)
 
-inputs = processor(image, return_tensors="pt").to(device, torch.float16)
+inputs = processor(image, return_tensors="pt").to("cpu", torch.float16)
 
 generated_ids = model.generate(**inputs, max_new_tokens=20)
 generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
