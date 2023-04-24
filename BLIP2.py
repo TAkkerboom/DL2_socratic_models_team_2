b9da4f4b682c8ce3d6066bda8f4959b01617b947
TAkkerboom <theodoor.akkerboom@wur.nl>
2023-04-24 17:21:03 +0200

    BLIP2 test

--- BLIP2.py
+++ BLIP2.py
@@ -1,15 +1,15 @@
 import requests
 from PIL import Image
-from transformers import AutoProcessor, Blip2ForConditionalGeneration, BitsAndBytesConfig
+from transformers import AutoProcessor, Blip2ForConditionalGeneration
 import torch
 
 url = 'https://media.newyorker.com/cartoons/63dc6847be24a6a76d90eb99/master/w_1160,c_limit/230213_a26611_838.jpg'
 image = Image.open(requests.get(url, stream=True).raw).convert('RGB') 
 processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
-model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16, device_map={"": 'cpu'}, load_in_8bit=True)
+model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16)
 
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-# model.to(device)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
 
 inputs = processor(image, return_tensors="pt").to("cpu", torch.float16)
 
