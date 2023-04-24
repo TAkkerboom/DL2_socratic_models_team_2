5b51b1fa50726f9e77dbc043e7892b25d036e521
TAkkerboom <theodoor.akkerboom@wur.nl>
2023-04-24 18:59:09 +0200

    BLIP2 test

--- BLIP2.py
+++ BLIP2.py
@@ -8,12 +8,13 @@ url = 'https://media.newyorker.com/cartoons/63dc6847be24a6a76d90eb99/master/w_11
 image = Image.open(requests.get(url, stream=True).raw).convert('RGB')
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(device)
+
 processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
 model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", device_map="auto", torch_dtype=torch.float16)
 model.to(device)
 
 
-inputs = processor(image, return_tensors="pt").to("cpu", torch.float16)
+inputs = processor(image, return_tensors="pt").to(device, torch.float16)
 
 generated_ids = model.generate(**inputs, max_new_tokens=20)
 generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
