bbd94a21ead573840952a068eacda9f871d4c8b9
TAkkerboom <theodoor.akkerboom@wur.nl>
2023-05-01 22:28:24 +0200

    VLM research update with batches and count

--- VLM_research.py
+++ VLM_research.py
@@ -6,9 +6,6 @@ from os import listdir
 from sklearn.metrics import classification_report
 import xml.etree.ElementTree as ET
 from plot_raven import plot_raven
-import torch.nn as nn
-from torchvision import transforms
-from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
 
 class Dataloader:
@@ -58,78 +55,56 @@ class Raven:
     
     def forward(self):
         i = 0
-        for npz, xml in zip(self.npz_data, self.xml_data):
+        endimage = 100
+        for npz, xml in zip(self.npz_data[:endimage], self.xml_data[:endimage]):
             data_point = Dataloader('{}/{}'.format(self.path, npz), '{}/{}'.format(self.path, xml))
             data_point.process()
             self.images.append(data_point.ravensmall)
             self.groundtruthshapes.append(data_point.groundtruthshapes)
             i += 1
             print("Loading image :{}".format(i))
-            if i == 100:
-                break
         pass
 
 
 class VLM:   
     def __init__(self):
-        print("Loading VLM")
+        # print("Loading VLM")
         self.processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
         self.model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
-
-        print("LM")
-        self.tokenizer = T5Tokenizer.from_pretrained("allenai/unifiedqa-v2-t5-large-1251000")
-        self.modelT5 = T5ForConditionalGeneration.from_pretrained("allenai/unifiedqa-v2-t5-large-1251000")
-    
-    def run_model(self, input_string, max_new_tokens=50):
-        with torch.no_grad():
-            input_ids = self.tokenizer.encode(input_string, return_tensors="pt")
-            res = self.modelT5.generate(input_ids.to(self.model.device), max_new_tokens=max_new_tokens)
-            return self.tokenizer.batch_decode(res, skip_special_tokens=True)
-        
+   
     def forward_caption(self, image, text):
-        print("Create caption")
+        # print("Create caption")
         encoding = self.processor(image, text, return_tensors="pt")
         outputs = self.model(**encoding)
         logits = outputs.logits
-        idx = logits.argmax(-1).item()
-
-        return self.model.config.id2label[idx]
-    
-    def forward_multiple_choice(self, text, choices, caption):
-        print("Create multiple choice")
-        prompt = f"please describe this image according to the given question: {text}"
-      
-        if len(choices) > 0:
-            choice_text = ""
-            for i in range(len(choices)):
-                choice_text += f"({i+1}) {choices[i]} "
-        print(caption)
-        return self.run_model(f"{prompt}. The object according to the VLM is equal to: {caption}. What is the shape of the object? You can only choose the following choices: {choice_text}?")
+        idxs = [logit.argmax(-1).item() for logit in logits]
+        return [self.model.config.id2label[idx] for idx in idxs]
 
     def forward(self, image):
-        print("Final VLM prediction")
         text = "What is the shape in the object?"
-        choices = ["none", "triangle", "square", "pentagon", "hexagon", "circle"]
-        caption = self.forward_caption(image, text)
-        return self.forward_multiple_choice(text, choices, str(caption))
+        textbatch = [text for i in range(len(image))]
+        return self.forward_caption(image, textbatch)
 
 def inference(dataset, model):
     count = 0
     predictions = []
-    amount_of_crops = 8
+    groundtruths = []
     types = ["none", "triangle", "square", "pentagon", "hexagon", "circle"]
-    print("Inference of the model")
-    for i, image in enumerate(zip(dataset.images, dataset.groundtruthshapes)):
-        image, groundtruth = image
-        for j in range(amount_of_crops):
-            out = model.forward(Image.fromarray(image[j]).convert("RGB"))[0]
-            print(out)
-            if out in types:
-                pred = types.index(out)
-                count += int(pred == groundtruth[j][0])
-                print(count)
-                predictions.append(pred)
-    return count, predictions
+    # print("Inference of the model")
+    count = 0 
+    for index, image_groundtruth in enumerate(zip(dataset.images, dataset.groundtruthshapes)):
+        image, groundtruth = image_groundtruth
+        convertimage = [Image.fromarray(single_image).convert("RGB") for single_image in image]
+        out = model.forward(convertimage)
+        groundtruthclass = [types[g[0]] for g in groundtruth]
+        count += sum(a == b for a, b in zip(out, groundtruthclass))
+        print(f"The count is equal to:{count} for image {index}")
+        for output,gclass in zip(out,groundtruthclass):
+            predictions.append(output)
+            groundtruths.append(gclass)
+    np.savez('VilT_performance', predictions=np.array(predictions), targets=np.array(groundtruths))
+
+    return groundtruths, predictions
 
 
 def main():
@@ -138,8 +113,9 @@ def main():
     dataset.forward()
     print("Loading Model")
     model = VLM()
-    count, pred = inference(dataset, model)
-    print(classification_report(dataset.groundtruthshapes, pred))
+    types = ["none", "triangle", "square", "pentagon", "hexagon", "circle"]
+    groundtruths, pred = inference(dataset, model)
+    print(classification_report(groundtruths, pred, labels=types))
 
 
 if __name__ == '__main__':
