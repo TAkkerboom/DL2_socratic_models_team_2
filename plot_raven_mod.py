b53b1989b1c1a3285627f73dd6ea9ad67d408dd4
TAkkerboom <theodoor.akkerboom@wur.nl>
2023-05-02 18:08:05 +0200

    Little update

--- plot_raven_mod.py
+++ plot_raven_mod.py
@@ -1,21 +1,14 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-
-# specify the amount of images you want to see
-IMGS = 1
-
-# Read the npz file
-for i in range(IMGS):
-    data = np.load('RAVEN-10000\center_single\RAVEN_' + str(i) + '_train.npz')
-
+def plot_raven(data):
     # Access the data in the npz file
     data_array = data['image']
 
     ## QUESTION
     # Create empty array to store the images
     image_grid = np.zeros((480, 480))
-
+    seperateimages=[]
     # Create a loop to go through each image
     for j in range(8):
         # Extract each image from the dataset
@@ -30,19 +23,22 @@ for i in range(IMGS):
         end_j = start_j + 160
         # Place the image in the empty array
         image_grid[start_i:end_i, start_j:end_j] = img
+        seperateimages.append(img)
 
     # Plot the final image
-    plt.title("Puzzle {}".format(i))
-    plt.imshow(image_grid)
-    plt.show()
+    # plt.title("Puzzle {}".format(0))
+    # plt.imshow(image_grid)
+    # plt.show()
+    image = image_grid
 
-    fig, axes = plt.subplots(1, 8, figsize=(20, 15))
-    plt.title("Answers {}".format(i))
-    for j in range(8):
-        axes[j].imshow(data_array[-8+j, :, :])
-    plt.show()
+    # fig, axes = plt.subplots(1, 8, figsize=(20, 15))
+    # plt.title("Answers {}".format(0))
+    # for j in range(8):
+    #     axes[j].imshow(data_array[-8+j, :, :])
+    # plt.show()
 
-    image_grid[320:480, 320:480] = data_array[8+data['target']]
-    plt.title('Solution {}'.format(i))
-    plt.imshow(image_grid)
-    plt.show()
+    # image_grid[320:480, 320:480] = data_array[8+data['target']]
+    # plt.title('Solution {}'.format(0))
+    # plt.imshow(image_grid)
+    # plt.show()
+    return seperateimages, image
