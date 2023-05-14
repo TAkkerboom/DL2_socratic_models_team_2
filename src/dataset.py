4cca038070ec07de4b163424429ae405054afca4
Sergei <mailbox.sa02@gmail.com>
2023-05-14 13:27:48 +0200

    VLM

--- src/dataset.py
+++ src/dataset.py
@@ -1,49 +1,71 @@
 import os
 import glob
 import numpy as np
-import gc
 import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
+from PIL import Image
+from torchvision import transforms
+from src.const import SHAPES, ANGLES, SIZES, COLORS
+
 
 class Raven:
     def __init__(self, root='./dataset/RAVEN-10000/', split='test', fig_type='*'):
-        
         self.npz_data = [f for f in glob.glob(os.path.join(root, fig_type, '*.npz')) if split in f]
         self.xml_data = [f for f in glob.glob(os.path.join(root, fig_type, '*.xml')) if split in f]
-        self.data = []
+        self.split = split
+        self.fig_type = fig_type
+        self.items = []
         
     def len(self):
-        return len(self.npz_data)
+        return len(self.items)
+    
+    def get_puzzle(self, id):
+        item = self.items[id]
+        images = item.images[0:16, :, :]
+        puzzle = []
+        
+        for image in images:
+            img = Image.fromarray(image).convert("P")
+            puzzle.append(img)
+            
+        return puzzle
+
+    def plot_puzzle(self, id):
+        item = self.items[id]
+        item.plot()
+
+    def get_answers(self, id):
+        item = self.items[id]
+        
+        return item.symbolic
     
     def load_data(self):
         for i, (npz, xml) in enumerate(zip(self.npz_data, self.xml_data)):
-            item = Dataloader(npz, xml, i)
+            item = Item(npz, xml, i)
             item.process()
             
-            self.data.append(item)
+            self.items.append(item)
             print(f"Loading item: {i}")
             break
             
-
-class Dataloader:
+class Item:
     def __init__(self, npz_path, xml_path, id):
         self.xml_path = xml_path
         self.npz_path = npz_path
-        self.id = id
+        self.puzzle_id = id
         
-    def get_image(self):
-        self.data = np.load(self.npz_path)
+        npz_data = np.load(self.npz_path)
+        self.images = npz_data['image']
+        self.target_id = npz_data['target']
+        self.symbolic = {}
         
+    def get_image(self):        
         # Create empty array to store the images
         grid = np.zeros((480, 480))
-        images = []
         
-        # Create a loop to go through each image
         for j in range(8):
             # Extract each image from the dataset
-            img = self.data['image'][j]
-            # Reshape each image to (160, 160)
-            img = img.reshape(160, 160)
+            img = self.images[j]
             # Calculate the starting index of each image
             start_i = (j // 3) * 160
             start_j = (j % 3) * 160
@@ -52,54 +74,49 @@ class Dataloader:
             end_j = start_j + 160
             # Place the image in the empty array
             grid[start_i:end_i, start_j:end_j] = img
-            images.append(img)
             
-        self.images, self.grid = images, grid
+        self.grid = grid
 
-    def ground_truth_shape(self):
-        # parse the xml file
+    def get_ground_truth(self):
+        # Parse the xml file
         tree = ET.parse(self.xml_path)
-
-        # access the root of the xml tree
+        # Access the root of the xml tree
         root = tree.getroot()
-
-        shapes = []
         
-        # iterate through the panels
-        for panel in root[0].findall('Panel'):
-            # find the component 
-            component = panel.find('Struct/Component')
-            # find the entity
-            entity = component.find('Layout/Entity')
-            # access the variables
-            type = int(entity.get('Type'))
-            # print the variables
-            shapes.append([type])
+        for i in range(16):
+            data = root[0][i][0][0][0][0].attrib
+            symbolic = {}
             
-        self.shapes = shapes[:8]
+            symbolic['Angle'] = ANGLES[data['Angle']]
+            symbolic['Color'] = COLORS[data['Color']]
+            symbolic['Size'] = SIZES[data['Size']]
+            symbolic['Type'] = SHAPES[data['Type']]
+            
+            self.symbolic[i] = symbolic
         
     def plot(self):
-        # Plot the final image
-        plt.title(f"Puzzle {self.id}")
+        name = self.npz_path.split('/')[-1]
+        
+        plt.title(f"Puzzle: {name}")
         plt.imshow(self.grid)
         plt.axis('off')
         plt.show()
 
         fig, axes = plt.subplots(1, 8, figsize=(20, 15))
-        plt.title(f"Answers {self.id}")
+        plt.title(f"Answers: {name}")
         for j in range(8):
-            axes[j].imshow(self.data['image'][-8+j, :, :])
+            axes[j].imshow(self.images[-8+j, :, :])
             axes[j].set_axis_off()
             
         plt.axis('off')
         plt.show()
 
-        self.grid[320:480, 320:480] = self.data['image'][8+self.data['target']]
-        plt.title(f'Solution {self.id}')
+        self.grid[320:480, 320:480] = self.images[8+self.target_id]
+        plt.title(f'Solution:{name}')
         plt.imshow(self.grid)
         plt.axis('off')
         plt.show()
     
     def process(self):
         self.get_image()
-        self.ground_truth_shape()
+        self.get_ground_truth()
--- src/model.py
+++ src/model.py
@@ -1,14 +1,16 @@
 from transformers import BlipProcessor, BlipForQuestionAnswering
+import torch
 
 class VLM:
     def __init__(self, model_name="Salesforce/blip-vqa-base", device="cpu"):
-        self.device = device
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.processor = BlipProcessor.from_pretrained(model_name)
-        self.model = BlipForQuestionAnswering.from_pretrained(model_name).to(device)
-    
+        self.model = BlipForQuestionAnswering.from_pretrained(model_name)
+        self.model.to(self.device)
+        
     def forward(self, image, prompt):
         inputs = self.processor(image, prompt, return_tensors="pt").to(self.device)
-        out = self.model.generate(**inputs)
+        out = self.model.generate(**inputs, max_length=50)
         out = self.processor.decode(out[0], skip_special_tokens=True)
         
-        return out
+        return out  
