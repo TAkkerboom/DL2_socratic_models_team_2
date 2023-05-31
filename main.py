1a4173bd9669e42353b3a8a031bc920acdff61d5
Marius Strampel <marius.strampel@gmail.com>
2023-05-31 16:45:12 +0200

    Auto stash before merge of "main" and "origin/main"
    Classification report support

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import argparse
 import torch
 import gdown
 import zipfile
+from sklearn.metrics import classification_report
+import json
 
 
 URL = "https://drive.google.com/file/d/111swnEzAY2NfZgeyAhVwQujMjRUfeyuY/view?usp=sharing"
@@ -39,11 +41,11 @@ class SM:
                 size = self.VLM.forward(c_image, 'What is the size of the figure? The options are: 0.4, 0.5, 0.6, 0.7, 0.8, 0.9')
                 type = self.VLM.forward(c_image, 'What is the shape of the figure? The options are: triangle, square, pentagon, hexagon, circle')
                 puzzle_answers.append([angle, color, size, type])
-                
+
             answers.append(puzzle_answers)
 
         return answers
-    
+
     def OpenCV_pred_attributes(self):
         answers = []
         opencvmodel = OpenCV()
@@ -54,7 +56,7 @@ class SM:
                 puzzle_answers.append(opencvmodel.detect_shape(c_image))
             answers.append(puzzle_answers)
         return answers
-            
+
     def generate_prompts(self, attributes):
         return [self.prompt.format(*[generate_single_description(*shape) for shape in puzzle]) for puzzle in attributes]
 
@@ -71,8 +73,8 @@ class SM:
     def inference(self, prompts, descriptions, batch_size, samples):
         predictions = []
         j = 0
-        prompts = [prompts[x:x+batch_size] for x in range(0, len(prompts), batch_size)] 
-        descriptions = [prompts[x:x+batch_size] for x in range(0, len(descriptions), batch_size)] 
+        prompts = [prompts[x:x+batch_size] for x in range(0, len(prompts), batch_size)]
+        descriptions = [prompts[x:x+batch_size] for x in range(0, len(descriptions), batch_size)]
         N  = int(samples / batch_size)
         for description_batch, prompt_batch in zip(descriptions[:N], prompts[:N]):
             out = self.LM.forward(prompt_batch)
@@ -85,7 +87,7 @@ class SM:
             j += 1
             if j % 1 == 0:
                 print('currently at ', j * 10)
-            
+
         return predictions
 
     def forward(self):
@@ -103,36 +105,36 @@ class Demo:
 
     def load_VLM(self, model):
         print(f'loading {model}...')
-        
+
         if model == 'openai/clip-vit-base-patch32':
             self.VLM = CLIP(model, self.device)
-            
+
             # Templates for predictions
             self.template_type = ['The figure is shaped as a {}'.format(value) for value in SHAPES.values()]
             self.template_color = ['The color of the figure is {}'.format(value) for value in COLORS.values()]
             self.template_angle = ['The figure is rotated by {} degrees'.format(value) for value in ANGLES.values()]
             self.template_sizes = ['The percentage of the image covered by the figure is {}'.format(value) for value in SIZES.values()]
-        
+
         else:
             self.VLM = VLM(model, self.device)
-            
+
             # Templates for predictions
             self.template_type = 'What is the shape of the figure?'
             self.template_color = 'What is the color of the figure?'
             self.template_angle = 'At what angle is the figure located?'
             self.template_sizes = 'What is the size of the figure?'
-        
+
         print('VLM is ready to use')
-        
+
     def load_LM(self, model, model_class=AutoModelForSeq2SeqLM):
         print(f'loading {model}...')
         self.LM = LM(model, model_class, self.device)
 
         print('LM is ready to use')
-        
+
     def get_attributes(self, puzzle):
         answers = []
-                
+
         for c_image in puzzle:
             if isinstance(self.template_angle, list):
                 angle = (self.VLM.forward(c_image, self.template_angle)).split()[-2]
@@ -144,26 +146,26 @@ class Demo:
                 color = self.VLM.forward(c_image, self.template_color)
                 size = self.VLM.forward(c_image, self.template_sizes)
                 type = self.VLM.forward(c_image, self.template_type)
-                
+
             answers.append([angle, color, size, type])
-            
+
         return answers
 
     def generate_prompts(self, attributes):
         desc = [generate_single_description(*attr) for attr in attributes]
         prompt = self.prompt.format(*desc)
-        
+
         return desc, prompt
-    
+
     def OpenCV_pred_attributes(self, puzzle):
         answers = []
         opencvmodel = OpenCV()
-        
+
         for c_image in puzzle:
             answers.append(opencvmodel.detect_shape(c_image))
-            
+
         return answers
-            
+
     def get_descriptions(self, attributes):
         descriptions = []
         for puzzle in attributes:
@@ -176,7 +178,7 @@ class Demo:
 
     def solve(self, prompt):
         pred = self.LM.forward(prompt)
-        
+
         return pred
 
     def forward(self, puzzle, ClassicOpenCV):
@@ -186,24 +188,24 @@ class Demo:
             attributes = self.get_attributes(puzzle)
         desc, prompt = self.generate_prompts(attributes)
         pred = self.solve([prompt])[0]
-        
+
         try:
             output = pred.split(',')[0]
         except:
             output = pred
 
         answers = desc[8:16]
-        
+
         if output in answers:
             index = answers.index(output)
         else:
             index = 8
-            
+
         return index, output
 
 def generate_single_description(angle, color, size, type):
     template = 'a {} {} of size {} at a {} degrees angle'.format(color, type, size, angle)
-    
+
     return template
 
 def set_seed(seed):
@@ -217,8 +219,44 @@ def set_seed(seed):
         torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    
-def main(name, seed, data_dir, split, type, ClassicOpenCV, vlm, lm):
+
+def evaluate_model(output_dir, name, groundtruths, preds):
+    x = [x[0] for x in groundtruths]
+    y = [y[0] for y in preds]
+
+    cr = classification_report(y, x, output_dict=True)
+
+    with open(output_dir + f'{name}_cr.txt', 'w') as f:
+        print(cr, file=f)
+
+def eval_model_from_file(output_dir, name, groundtruths_path, preds_path):
+    with open(groundtruths_path, 'r') as gt, open(preds_path, 'r') as p:
+        groundtruths, preds = [], []
+        for gt_line, pred_line in zip(gt, p):
+            gt_content, pred_content = gt_line.strip(), pred_line.strip()
+
+            if gt_content[2] == ',':
+                id = int(gt_content[1])
+                desc = json.loads(gt_content[3:-1].replace("'", '"'))
+            else:
+                id = int(gt_content[1:3])
+                desc = json.loads(gt_content[4:-1].replace("'", '"'))
+
+            groundtruths.append((id, desc))
+
+            if pred_content[2] == ',':
+                id = int(pred_content[1])
+                desc = json.loads(pred_content[3:-1].replace("'", '"'))
+            else:
+                id = int(pred_content[1:3])
+                desc = json.loads(pred_content[4:-1].replace("'", '"'))
+
+            preds.append((id, desc))
+
+        evaluate_model(output_dir, name, groundtruths, preds)
+
+
+def main(name, seed, data_dir, split, type, ClassicOpenCV, vlm, lm, eval=True):
     """
     Main function for testing the model.
     """
@@ -226,7 +264,7 @@ def main(name, seed, data_dir, split, type, ClassicOpenCV, vlm, lm):
     set_seed(seed)
     # Pick GPU if available
     device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
-    
+
     # Download the RAVEN dataset
     output = f"{data_dir}RAVEN10000.zip"
     if not exists(output):
@@ -238,27 +276,43 @@ def main(name, seed, data_dir, split, type, ClassicOpenCV, vlm, lm):
             print('unpacking done')
     else:
         print('Dataset already exists, moving further...')
-        
+
     # Load the data
     test_set = Raven(f'{data_dir}RAVEN-10000/', split, type)
     test_set.load_data()
-    
+
     # Prepare the Socratic Model
     model = Demo(device)
     model.load_VLM(vlm)
     model.load_LM(lm)
-    
-    with open(f'./output/{name}_results.txt', 'w') as file:
+
+    output_dir = f'./output/'
+
+    groundtruths = []
+    preds = []
+
+    # if eval:
+    #     eval_model_from_file(output_dir, name, './output/gt_centresingle_test.txt', './output/CLIP_FT5XL.txt')
+
+    with open(output_dir + f'{name}_results.txt', 'w') as file:
         # Inference
         for i in tqdm(range(1)):
             if not ClassicOpenCV:
                 puzzle = test_set.get_puzzle(i)
             else:
                 puzzle = test_set.items[i].images[0:16, :, :]
+
             index, output = model.forward(puzzle,ClassicOpenCV)
             file.write(f"{index, output}\n")
-        
-    file.close()
+
+            if eval:
+                id = test_set.items[i].target_id
+                groundtruths.append((id, test_set.items[i].symbolic[id]))
+                preds.append((index, output))
+
+        if eval:
+            evaluate_model(output_dir, name, groundtruths, preds) # Optional: add metrics specifier argument
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -279,7 +333,9 @@ if __name__ == '__main__':
                         help='VLM weights to use.')
     parser.add_argument('--lm', default='google/flan-t5-xl', type=str,
                         help='LM weights to use.')
-    
+    parser.add_argument('--eval', default=True, type=bool,
+                        help='Evaluate SM model after inference.')
+
     args = parser.parse_args()
     kwargs = vars(args)
     main(**kwargs)
--- src/dataset.py
+++ src/dataset.py
@@ -15,23 +15,23 @@ class Raven:
         self.split = split
         self.fig_type = fig_type
         self.items = []
-        
+
     def len(self):
         return len(self.items)
-    
+
     def get_puzzle(self, id):
         item = self.items[id]
         images = item.images[0:16, :, :]
         puzzle = []
-        
+
         for i, image_array in enumerate(images):
             image = Image.fromarray(image_array.astype(np.uint8), mode='L')
             image = image.convert('RGB')
-            
+
             width, height = image.size
-            
+
             draw = ImageDraw.Draw(image)
-            
+
             for y in range(height):
                 for x in range(width):
                     if image_array[y, x] != 255:
@@ -44,7 +44,7 @@ class Raven:
             draw.line([(width // 2, 0), (width // 2, height - 1)], fill=(0, 0, 0), width=1)
 
             puzzle.append(image)
-            
+
         return puzzle
 
     def plot_puzzle(self, id):
@@ -53,119 +53,119 @@ class Raven:
 
     def get_answers(self, id):
         item = self.items[id]
-        
+
         return item.symbolic
-    
+
     def load_data(self):
         print('Loading data...')
         for i, (npz, xml) in enumerate(zip(self.npz_data, self.xml_data)):
             item = Item(npz, xml, i)
-            item.process()            
+            item.process()
             self.items.append(item)
 
         print(f'{len(self.items)} items were successfully loaded')
-            
+
 class Item:
     def __init__(self, npz_path, xml_path, id):
         self.xml_path = xml_path
         self.npz_path = npz_path
         self.puzzle_id = id
-        
+
         npz_data = np.load(self.npz_path)
         self.images = npz_data['image']
-        self.target_id = npz_data['target']
+        self.target_id = npz_data['target'].item()
         self.symbolic = {}
         self.colors = []
         self.puzzle_image = None
-        
+
     def get_ground_truth(self):
         # Parse the xml file
         tree = ET.parse(self.xml_path)
         # Access the root of the xml tree
         root = tree.getroot()
-        
+
         for i in range(16):
             data = root[0][i][0][0][0][0].attrib
             symbolic = {}
-            
+
             symbolic['Type'] = SHAPES[data['Type']]
             symbolic['Color'] = COLORS[data['Color']]
-            symbolic['Angle'] = ANGLES[data['Angle']]            
+            symbolic['Angle'] = ANGLES[data['Angle']]
             symbolic['Size'] = SIZES[data['Size']]
-            
+
             self.colors.append(NAME_TO_COLOR[symbolic['Color']])
             self.symbolic[i] = symbolic
-                
+
     def generate_matrix(self, array_list):
         # row-major array_list
         array_list = array_list[0:8, :, :]
         img_grid = np.zeros((IMAGE_SIZE * 3, IMAGE_SIZE * 3), np.uint8)
-        
+
         for idx in range(len(array_list)):
             i, j = divmod(idx, 3)
             img_grid[i * IMAGE_SIZE:(i + 1) * IMAGE_SIZE, j * IMAGE_SIZE:(j + 1) * IMAGE_SIZE] = array_list[idx]
-            
+
         # draw grid
         for x in [0.33, 0.67]:
             img_grid[int(x * IMAGE_SIZE * 3) - 1:int(x * IMAGE_SIZE * 3) + 1, :] = 0
         for y in [0.33, 0.67]:
             img_grid[:, int(y * IMAGE_SIZE * 3) - 1:int(y * IMAGE_SIZE * 3) + 1] = 0
-            
+
         return img_grid
 
     def generate_answers(self, array_list):
         array_list = array_list[8:16, :, :]
         img_grid = np.zeros((IMAGE_SIZE * 2, IMAGE_SIZE * 4), np.uint8)
-        
+
         for idx in range(len(array_list)):
             i, j = divmod(idx, 4)
             img_grid[i * IMAGE_SIZE:(i + 1) * IMAGE_SIZE, j * IMAGE_SIZE:(j + 1) * IMAGE_SIZE] = array_list[idx]
-            
+
         # draw grid
         for x in [0.5]:
             img_grid[int(x * IMAGE_SIZE * 2) - 1:int(x * IMAGE_SIZE * 2) + 1, :] = 0
         for y in [0.25, 0.5, 0.75]:
             img_grid[:, int(y * IMAGE_SIZE * 4) - 1:int(y * IMAGE_SIZE * 4) + 1] = 0
-            
+
         return img_grid
 
     def generate_matrix_answer(self, array_list):
         # row-major array_list
         assert len(array_list) <= 18
         img_grid = np.zeros((IMAGE_SIZE * 6, IMAGE_SIZE * 3), np.uint8)
-        
+
         for idx in range(len(array_list)):
             i, j = divmod(idx, 3)
             img_grid[i * IMAGE_SIZE:(i + 1) * IMAGE_SIZE, j * IMAGE_SIZE:(j + 1) * IMAGE_SIZE] = array_list[idx]
-            
+
         # draw grid
         for x in [0.33, 0.67, 1.00, 1.33, 1.67]:
             img_grid[int(x * IMAGE_SIZE * 3), :] = 0
         for y in [0.33, 0.67]:
             img_grid[:, int(y * IMAGE_SIZE * 3)] = 0
-            
+
         return img_grid
-    
+
     def merge_matrix_answer(self, array_list):
         matrix_image = self.generate_matrix(array_list)
         answer_image = self.generate_answers(array_list)
-        
+
         img_grid = np.ones((IMAGE_SIZE * 5 + 20, IMAGE_SIZE * 4), np.uint8) * 255
         img_grid[:IMAGE_SIZE * 3, int(0.5 * IMAGE_SIZE):int(3.5 * IMAGE_SIZE)] = matrix_image
         img_grid[-(IMAGE_SIZE * 2):, :] = answer_image
-        
+
         return img_grid
 
     def process(self):
         array = self.images
         self.merged = self.merge_matrix_answer(array)
         self.puzzle_image = self.generate_matrix(array[:8])
-        
+
         self.get_ground_truth()
-        
+
     def plot(self):
         name = self.npz_path.split('/')[-1]
-        
+
         plt.title(f"Puzzle: {name}")
         plt.imshow(self.merged)
         plt.axis('off')
